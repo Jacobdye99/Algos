5c833c19422379ad4055ba9b5fe6d6cfd723cb4c
Jacobdye99 <Jacobdye99@gmail.com>
2022-03-25 13:27:48 -0400

    square

---
+++ Walk.rb
@@ -0,0 +1,10 @@
+def is_valid_walk(walk)
+    #your code here
+    if walk.length == 10 
+      p "Ten steps"
+    else
+        p "not 10 minutes"
+    end
+  end
+
+  is_valid_walk(['n','s','n','s'])
